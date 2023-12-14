72e16b9494e6fb3ac62d907cd434bc8b16cdc583
Umang Rathod <umang00775@gmail.com>
2023-12-14 14:15:05 +0530

    CORS, cookie-parse configured

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parse"
+
+const app = express();
+
+// app.use(cors());  /* This will also work, but not professional */
+
+// Cors set up
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,  // Allowed URLs to use (Front end :) )
+    credentials: true  // Allow credentials
+}))
+
+app.use(express.json({limit:"16kb"}))  // How much data at once 
+
+app.use(express.urlencoded({extended: true, limit:"16kb"})) // To allow object inside object (Nested objects)
+
+app.use(express.static("public")) // Use static files
+
+app.use(cookieParser()) // For cookies use
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js"; 
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
-
+.then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+        console.log(`Connected to port ${port}`);
+    })
+})
+.catch((error) => {
+    console.log("Connection failed!! \n", error);
+})
 
 
 

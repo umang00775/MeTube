b354c300c222b2215c7072eb71457fe94b17ca7b
Umang Rathod <umang00775@gmail.com>
2024-01-01 11:09:41 +0530

    Router and Controller started

--- src/app.js
+++ src/app.js
@@ -13,11 +13,11 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))  // How much data at once 
-
 app.use(express.urlencoded({extended: true, limit:"16kb"})) // To allow object inside object (Nested objects)
-
 app.use(express.static("public")) // Use static files
-
 app.use(cookieParser()) // For cookies use
+
+// Import routes and controllers
+import router from "./routes/user.routes.js";
 
 export {app}
---
+++ src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router()
+
+// Code
+
+export default router
